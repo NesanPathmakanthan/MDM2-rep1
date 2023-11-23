6bdc8406a125d49791753fa5b09ccb5c2dffa64b
Nesan <102155836+NesanPathmakanthan@users.noreply.github.com>
2023-11-23 11:09:10 +0000

    Add files via upload

--- generate_networks.m
+++ generate_networks.m
@@ -5,29 +5,58 @@ P_email = 1 - P_email;
 P_letter = 1 - P_letter;
 P_radio = 1 - P_radio;
 
-phone = rand(5);
-phone = (phone + phone')/2;
-phone = phone - diag(diag(phone));
+selective_nodes = randperm(70,3);
+email_only_node = selective_nodes(1);
+letter_only_node = selective_nodes(2);
+radio_only_node = selective_nodes(3);
+
+phone = zeros(70);
+phone1 = rand(25);
+phone1 = (phone1 + phone1')/2;
+phone1 = phone1 - diag(diag(phone1));
+phone2 = rand(45);
+phone2 = (phone2 + phone2')/2;
+phone2 = phone2 - diag(diag(phone2));
+phone(1:25, 1:25) = phone1;
+phone(26:70, 26:70) = phone2;
 phone(phone >= P_phone) = 1;
 phone(phone < P_phone) = 0;
+phone(letter_only_node,:) = 0;
+phone(:,letter_only_node) = 0;
+phone(radio_only_node,:) = 0;
+phone(:,radio_only_node) = 0;
+phone(email_only_node,:) = 0;
+phone(:,email_only_node) = 0;
 
-email = rand(5);
+email = rand(70);
 email = (email + email')/2; 
 email = email - diag(diag(email));
 email(email >= P_email) = 1;
 email(email < P_email) = 0;
+email(letter_only_node,:) = 0;
+email(:,letter_only_node) = 0;
+email(radio_only_node,:) = 0;
+email(:,radio_only_node) = 0;
 
-letter = rand(5);
+letter = rand(70);
 letter = (letter + letter')/2;
 letter = letter - diag(diag(letter));
 letter(letter >= P_letter) = 1;
 letter(letter < P_letter) = 0;
+letter(email_only_node,:) = 0;
+letter(:,email_only_node) = 0;
+letter(radio_only_node,:) = 0;
+letter(:,radio_only_node) = 0;
 
-radio = rand(5);
+radio = rand(70);
 radio = (radio + radio')/2;
 radio = radio - diag(diag(radio));
 radio(radio >= P_radio) = 1; 
 radio(radio <  P_radio) = 0;
+radio(letter_only_node,:) = 0;
+radio(:,letter_only_node) = 0;
+radio(email_only_node,:) = 0;
+radio(:,email_only_node) = 0;
 
 % phone
 % email
@@ -35,11 +64,11 @@ radio(radio <  P_radio) = 0;
 % radio
 
 % hold on
-plot(graph(phone))
+% plot(graph(phone))
 % plot(graph(email))
 % plot(graph(letter))
 % plot(graph(radio))
 % hold off
-joined_network = cat(3, phone, email, letter, radio)
+% joined_network = cat(3, phone, email, letter, radio);
 
 end
